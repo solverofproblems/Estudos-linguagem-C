5ed8d528d0b0960ec09e9b2579f8d481ee961833
Carlos Eduardo de Lima <carloselima373@gmail.com>
2026-03-01 21:09:52 -0300

    Aula 03 - 01/03/2026

--- codigo_realizado.cpp
+++ codigo_realizado.cpp
@@ -386,7 +386,298 @@ int main(){
 	cout << resp;
 	
 	
+	//Entendendo a estrutura Switch:
 	
+	/*
+		
+		Usamos a seguinte estrutura sintática:
+		
+		switch (variavel_desejada){
+		
+			case comparacao1:
+				comando;
+				break;
+				
+			case comparacao2:
+				comando;
+				break;
+				
+			case comparacao3:
+				comando;
+				break;
+				
+			default:
+				comando; //Aqui NÃO usamos "break".
+				
+		};
+	
+	*/
+	
+//	int choice;
+//	
+//	cout << "Selecione uma cor: \n";
+//	cout << "[1, 2, 3] - Verde ; [4, 5, 6] - Vermelho ; [7, 8, 9] - Azul\n";
+//	
+//	cin >> choice;
+//	
+//	switch (choice){
+//		
+//		case 1:
+//		case 2:
+//		case 3:
+//			cout << "Cor selecionada: Verde\n";
+//			break;
+//			
+//		case 4:
+//		case 5:
+//		case 6:
+//			cout << "Cor selecionada: Vermelho\n";
+//			break;
+//			
+//		case 7:
+//		case 8:
+//		case 9:
+//			cout << "Cor selecionada: Azul\n";
+//			break;
+//			
+//		default:
+//			cout << "Cor invalida!";
+//			
+//	}
+//	
+//	cout << "\nSistema funcionando adequadamente!\n";
+
+
+	int transport;
+	
+	cout << "\nSelecione um transporte: \n";
+	cout << "[1] - Carro; [2] - Moto; [3] - Aviao; [4] - Helicoptero"<<endl;
+	
+	cin >> transport;
+	
+	switch (transport){
+		
+		case 1:
+		case 2:
+			
+			cout << "\nTipo de transporte selecionado: Terrestre.\n";
+			
+			switch (transport){
+				
+				case 1:
+					cout << "Transporte selecionado: Carro.\n";
+					break;
+				case 2:
+					cout << "Transporte selecionado: Moto.\n";
+					break;
+				
+			}
+			
+			break;
+
+		case 3:
+		case 4:
+			
+			cout << "\nTipo de transporte selecionado: Aereo.\n";
+			
+			switch (transport){
+				
+				case 3:
+					cout << "Transporte selecionado: Aviao.\n";
+					break;
+				case 4:
+					cout << "Transporte selecionado: Helicoptero.\n";
+					break;
+				
+			}
+			
+			break;
+		
+	};
+	
+	
+	//Estudando a estrutura de repetição "While".
+	
+	/*
+		
+		Sobre sua estrutura, ela funciona da seguinte forma:
+		
+		while(condicao){
+		
+			codigo;
+		
+		};
+	
+	
+	*/
+	
+	int contador;
+	
+	while(contador <= 20){ //A estrutura continuará acontecendo enquanto a condição retornar "true".
+	
+		cout << contador <<endl;
+		contador ++; // É fundamental que, em algum momento, a condição while deixe ser "true".
+		
+		if (contador == 10){
+			break; //Não permite que o contador seja maior do que 10.
+		};
+		
+		
+	}
+	
+	/*
+		Entendendo a estrutura Do While.
+		
+		Estrutura básica:
+		
+		do {
+			condicao;
+		} while(condicao);
+	
+	
+	*/
+	
+	int cont = 20;
+	
+	do {
+		
+		cout << cont << "\n";
+		cont ++;
+		
+	} while(cont < 20); //Veja que o código executou uma vez, mesmo que o cont já fosse igual a 20.
+	
+
+
+	/*Entendendo estrutura For.
+	
+	
+		for (variavel_desejada ; condicao; incremento/decremento){
+		
+			codigo_desejado;
+		
+		}
+	
+	*/
+	
+	int valores;
+	
+	for (int i = 0; i <= 20; i ++){
+		
+		cout <<i << "\n";
+		
+	}
+
+
+	int par, impar;
+	
+	for (par = 0, impar = 1; par <= 20; par+=2, impar+=2){
+		
+		cout << par << " - " << impar << "\n";	
+		
+	}
+	
+	cout << "Codigo finalizado!";
+
+
+	/*Entendendo arrays/vetores em C++:
+	
+	
+		Estrutura básica: 
+		
+		int nome_desejado[tamanho_desejado];
+	
+	
+	*/
+	
+	
+	string vetor[3]; //Definimos o vetor com tamanho 3.
+	
+	vetor[0] = "Banana"; //Os valores que colocamos nas chaves são os ÍNDICES.
+	vetor[1] = "Uva"; //Os valores que colocamos nas chaves são os ÍNDICES.
+	vetor[2] = "Abacate"; //Os valores que colocamos nas chaves são os ÍNDICES.
+	
+	//Por final, exibimos o valor que desejamos no vetor com o índice.
+	cout <<"\nA fruta selecionada foi: "<< vetor[0] <<endl;
+	
+	
+	//Agora vamos usar a estrutura For para isso.
+	
+	int tamanho_array = 3;
+	
+	for (int j = 0; j < tamanho_array; j++){
+		
+		cout << "A fruta: " << vetor[j] << " existe na cesta de frutas!\n";
+		
+	};
+	
+	//Exemplificando outra forma de criar Arrays:
+	
+	int outro_vetor[5] = {1,2,3,4,5};
+	cout << outro_vetor[0]<<endl;
+	
+	
+		/*
+	
+		Entendendo matrizes em C++.
+		
+		int matriz[3][4];
+		
+		Visualmente falando, temos isso:
+		
+		    0 1 2 3
+		0: [][][][]
+		1: [][][][]
+		2: [][][][x] //Linha 3, coluna 4. Em C++, seria matriz[2][3] = x;
+	
+		Importante: Note que o índice 0 é considerado na construção dessa tabela.
+	
+	*/
+	
+	
+	int matriz[3][4];
+	
+//	//Linha 1
+//	matriz[0][0] = 0;
+//	matriz[0][1] = 0;
+//	matriz[0][2] = 0;
+//	matriz[0][3] = 0;
+//	
+//	//Linha 2
+//	matriz[1][0] = 1;
+//	matriz[1][1] = 1;
+//	matriz[1][2] = 1;
+//	matriz[1][3] = 1;
+//	
+//	//Linha 3
+//	matriz[2][0] = 2;
+//	matriz[2][1] = 2;
+//	matriz[2][2] = 2;
+//	matriz[2][3] = 2;
+
+	for (int li=0; li < 3; li++){
+		
+		for (int co=0; co < 4; co++){
+			
+			cout << "Selecione um valor: ";
+			cin >> matriz[li][co];
+			cout << "\n";
+			
+		};
+		
+	};
+	
+	for (int l=0; l < 3; l++){
+		
+		for (int c=0; c < 4; c++){
+			
+			cout << matriz[l][c] << " ";		
+			
+		};
+		
+		cout << "\n";
+		
+	};
+
 	return 0;
-	
+		
 }
