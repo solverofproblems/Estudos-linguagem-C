3635ea77f428c9ff4801218c1715ac46b9360501
Carlos Eduardo de Lima <carloselima373@gmail.com>
2026-04-05 16:44:51 -0300

    OBI - Mania de Ímpar

---
+++ mania_de_impar_OBI.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	
+	int n, m, qntdAdd;
+	
+	cin >> n;
+	cin >> m;
+	
+	int matriz_cookie[n][m];
+	
+	for (int l=0; l < n; l++){
+		
+		for (int c=0; c < m; c++){
+			
+			cin >> matriz_cookie[l][c];
+			
+		};
+		
+	};
+	
+	
+	for (int l; l < n; l++){
+		
+		cout << "\n";
+		
+		for (int c=0; c < m; c++){
+			
+			
+			cout << matriz_cookie[l][c] << " ";
+			
+		};
+		
+	};
+	
+
+	for (int p = 0; p < 2; p++){
+		
+		
+		for (int l =0; l < n; l++){
+			
+			for (int c = 0; c < m; c++){
+				
+				bool deveSerImpar = ((l + c) % 2 == p);
+				bool ehImpar = (matriz_cookie[l][c] % 2 != p);
+				
+				if (deveSerImpar != ehImpar){
+					matriz_cookie[l][c]++;
+					qntdAdd++;
+				};	
+			};	
+		};
+	};
+		
+	cout << "\n\nQuantidade de gotas adicionadas: " << qntdAdd <<endl;
+		
+	for (int l; l < n; l++){
+		
+		cout << "\n";
+		
+		for (int c=0; c < m; c++){
+			
+			
+			cout << matriz_cookie[l][c] << " ";
+			
+		};
+		
+	};
+		
+
+
+	return 0;
+}
