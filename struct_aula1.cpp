b3a59aaad2f1f541d2b16152e48a8f1cf52a2913
Carlos Eduardo de Lima <carloselima373@gmail.com>
2026-04-05 11:29:38 -0300

    modificações - struct correta

--- struct_aula1.cpp
+++ struct_aula1.cpp
@@ -2,22 +2,29 @@
 
 using namespace std;
 
-//Para criar uma Struct, basta especificá-la, atribuir um nome a ela e as características
-//Que você deseja que tenha. Veja:
-
-struct Carro(
-	
+struct Carro{
+ 
 	string nomeCarro;
 	string corCarro;
 	int potCarro;
 	int velMax;
 
-);
+};
 
 int main (){
 	
+	Carro car1;
 	
-	cout << "olá, código base funcionando adequadamente.";
+	car1.nomeCarro = "Fiat Uno";
+	car1.corCarro = "Vermelho";
+	car1.potCarro = 450;
+	car1.velMax = 230;
+	
+	cout << "Nome.............: " << car1.nomeCarro <<endl;
+	cout << "Cor..............: " << car1.corCarro <<endl;
+	cout << "Potencia.........: " << car1.potCarro <<endl;
+	cout << "Velocidade Maxima: " << car1.velMax <<endl;
+	
 	
 	return 0;
 	
